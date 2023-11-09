e9250a42e884955a2ecac0c5b519c84834ba199c
saivamshikrishnagajji <137726056+saivamshikrishnagajji@users.noreply.github.com>
2023-11-09 00:59:31 -0600

    added code

--- controller/animal.js
+++ controller/animal.js
@@ -1,4 +1,5 @@
 var Animal = require('../models/animal');
+
 // List of all Animals
 //exports.animal_list = function(req, res) {
 //res.send('NOT IMPLEMENTED: Animal list');
@@ -21,9 +22,7 @@ exports.animal_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Animal detail: ' + req.params.id);
 };
 // Handle Animal create on POST.
-exports.animal_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Animal create POST');
-};
+
 // Handle Animal delete form on DELETE.
 exports.animal_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Animal delete DELETE ' + req.params.id);
@@ -32,3 +31,41 @@ res.send('NOT IMPLEMENTED: Animal delete DELETE ' + req.params.id);
 exports.animal_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Animal update PUT' + req.params.id);
 };
+
+//VIEWS
+// Handle a show all view
+exports.animal_view_all_Page = async function(req, res) {
+try{
+theAnimals = await Animal.find();
+res.render('animals', { title: 'Animal Search Results', results: theAnimals });
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+
+/* GET animals */
+
+exports.animal_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Animal();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"animal_type":"goat", "cost":12, "size":"large"}
+    document.animal_type = req.body.animal_type;
+    document.color = req.body.color;
+    document.legs = req.body.legs;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+
+
--- routes/animals.js
+++ routes/animals.js
@@ -1,9 +1,9 @@
 var express = require('express');
+const animal_controlers= require('../controller/animal');
 var router = express.Router();
-
-
-router.get('/', function(req, res, next) {
-  res.render('animals', { title: 'Search Results - Animals.' });
-});
-
+/* GET animals */
+router.get('/', animal_controlers.animal_view_all_Page );
 module.exports = router;
+
+
+
