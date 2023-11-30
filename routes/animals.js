9f28bff6da4f27b15ee445eb1e42edff9f2d9a84
saivamshikrishnagajji <137726056+saivamshikrishnagajji@users.noreply.github.com>
2023-11-29 19:10:05 -0600

    bonus added

--- routes/animals.js
+++ routes/animals.js
@@ -2,12 +2,19 @@ var express = require('express');
 const animal_controlers= require('../controller/animal');
 var router = express.Router();
 /* GET animals */
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 router.get('/', animal_controlers.animal_view_all_Page );
-router.get('/detail', animal_controlers.animal_view_one_Page);
-router.get('/create', animal_controlers.animal_create_Page);
+router.get('/detail', secured, animal_controlers.animal_view_one_Page);
+router.get('/create',secured, animal_controlers.animal_create_Page);
 
-router.get('/update', animal_controlers.animal_update_Page);
-router.get('/delete', animal_controlers.animal_delete_Page);
+router.get('/update',secured, animal_controlers.animal_update_Page);
+router.get('/delete',secured, animal_controlers.animal_delete_Page);
 
 
 module.exports = router;
